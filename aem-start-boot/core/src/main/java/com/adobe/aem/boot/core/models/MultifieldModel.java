bdbaa7ca1a2179571e35f2590821ac4ad76c79d8
Divya Rathi <rathidivya07@gmail.com>
2019-07-31 12:21:41 +0530

    Second Assignment

---
+++ aem-start-boot/core/src/main/java/com/adobe/aem/boot/core/models/MultifieldModel.java
@@ -0,0 +1,30 @@
+package com.adobe.aem.boot.core.models;
+
+import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
+import org.apache.sling.models.annotations.injectorspecific.OSGiService;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.settings.SlingSettingsService;
+
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+
+import org.apache.sling.models.annotations.Model;
+
+@Model(adaptables = Resource.class , defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class MultifieldModel {
+	
+	@Inject
+	public Resource products;
+
+}
